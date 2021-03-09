e3f97c5dab35e62c54c8a70c6a8a191eb2bb91c8
Кухтин Олег <Kuhtin_OK@akelon.com>
2021-03-09 09:03:26 +0300

    Refact

--- CurrencyRater/Controllers/HomeController.cs
+++ CurrencyRater/Controllers/HomeController.cs
@@ -33,11 +33,5 @@ namespace CurrencyRater.Controllers
             }
             return View("Index",valutesList);
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
